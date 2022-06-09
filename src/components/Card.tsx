2fae90e4eb5c00963abba5b30010823de058f79d
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-06-09 14:33:36 +0400

    create function components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,36 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+
+//перечисление что будет содержать props variant
+export enum CardVariant {
+  outlined = "outlined",
+  primary = "primary",
+}
 
 interface CardProps {
   width?: string;
   height?: string;
-  children?: React.ReactNode
+  children?: React.ReactNode;
+  variant: CardVariant;
 }
 // children - будет являться всё что внутри карточки
-const Card: FC <CardProps> = ({ width, height,children }) => {
+const Card: React.FC<CardProps> = ({
+  width,
+  height,
+  variant,
+  children,
+}) => {
+  const [state,setState] = useState(0)
   return (
-    <div style={{ width, height, border: "1px solid gray" }}>{children}</div>
+    <div
+      style={{
+        width,
+        height,
+        border: variant === CardVariant.outlined ? "1px solid gray" : "none",
+        background: variant === CardVariant.primary ? "Lightgray" : "",
+      }}
+    >
+      {children}
+    </div>
   );
 };
 
