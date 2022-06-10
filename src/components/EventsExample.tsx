267a1029ba89d188bfb189829f94b8fc7ba740a2
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-06-10 14:28:58 +0400

    типизировал хук useRef

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,48 +1,62 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useRef, useState } from "react";
 
-interface EventsExampleProps {
-
-}
+interface EventsExampleProps {}
 
 const EventsExample: FC<EventsExampleProps> = () => {
-    const [value , setValue] = useState<string>('')
-    const [isDrag,setIsDrag] = useState<boolean>(false)
+  const [value, setValue] = useState<string>("");
+  const [isDrag, setIsDrag] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-setValue(e.target.value)
-    }
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
 
-const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-console.log(value)
-}
-const dragHandler = (e:React.DragEvent<HTMLDivElement>) => {
-console.log('DRAG');
-}
-const dragWithPreventHandler = (e:React.DragEvent<HTMLDivElement>) => {
-e.preventDefault()
-setIsDrag(true)
-}
-const leaveHandler = (e:React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault()
-    setIsDrag(false)
-    }
-    const dropHandler = (e:React.DragEvent<HTMLDivElement>) => {
-        e.preventDefault()
-        setIsDrag(false)
-        console.log("DROP");
-        
-        }
+  const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(inputRef.current?.value);
+  };
+  const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    console.log("DRAG");
+  };
+  const dragWithPreventHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDrag(true);
+  };
+  const leaveHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDrag(false);
+  };
+  const dropHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDrag(false);
+    console.log("DROP");
+  };
   return (
     <div>
-        <input value={value} onChange={changeHandler} type="text"/>
-        <button onClick={clickHandler}>FOCK</button>
-        <div onDrag={dragHandler} draggable style={{width: 200, height: 200, background: 'red'}}></div>
-        <div
-                onDrop={dropHandler}
-                onDragLeave={leaveHandler}
-                onDragOver={dragWithPreventHandler}
-                style={{width: 200, height: 200, background: isDrag ? 'blue' : 'red', marginTop: 15}}></div>
+      <input
+        value={value}
+        onChange={changeHandler}
+        type="text"
+        placeholder="Управляемый"
+      />
+      <input ref={inputRef} type="text" placeholder="Неуправляемый" />
+      <button onClick={clickHandler}>FOCK</button>
+      <div
+        onDrag={dragHandler}
+        draggable
+        style={{ width: 200, height: 200, background: "red" }}
+      ></div>
+      <div
+        onDrop={dropHandler}
+        onDragLeave={leaveHandler}
+        onDragOver={dragWithPreventHandler}
+        style={{
+          width: 200,
+          height: 200,
+          background: isDrag ? "blue" : "red",
+          marginTop: 15,
+        }}
+      ></div>
     </div>
   );
-  };
-export default EventsExample
+};
+export default EventsExample;
